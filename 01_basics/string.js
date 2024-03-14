b629c837ef99313a514b2bf4737939356ed74dd8
pulkitt001 <136142497+pulkitt001@users.noreply.github.com>
2024-03-14 19:33:46 +0000

    string

--- 01_basics/string.js
+++ 01_basics/string.js
@@ -1,3 +1,25 @@
 const name = "pulkit"
 const repoCount=30
 console.log( `Hello my name is ${name} and my repo count is ${repoCount}` );
+
+const gamename = "pulkitt"
+const game= new String('mostly')
+
+console.log(gamename[0]);
+console.log(gamename.length);
+console.log(gamename.charAt(3));
+console.log(gamename.indexOf('t'));
+console.log(game)
+/*we can perform many action on the string just like , in uppercase,lowercase 
+
+*/
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+let nam = "Ansh Yadav";
+const age = 22;
+let infor = `hey guys! this is ${name} this side , and my age is ${age}`; //this is backtics method ,and variable holder is called place holder in it
+console.log(infor);
+
+console.log(name.substring(3,4)); //this has starting and ending index in the substring
+console.log(name.slice(0,3)); //start w
+// trim, replace ,includes etc
