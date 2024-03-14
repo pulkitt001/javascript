2c1a8c1ed1ec1a0c1bcde0f73d79478e2d1f3725
pulkitt001 <136142497+pulkitt001@users.noreply.github.com>
2024-03-14 19:30:54 +0000

    date

---
+++ 01_basics/dateAndTime.js
@@ -0,0 +1,11 @@
+let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toDateString());
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
+
+//the time we can get and access in different form
+let dd = new Date();
+console.log(dd.getDate());
+console.log(dd.getMonth());
